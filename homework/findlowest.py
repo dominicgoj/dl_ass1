8b8275d3ff0f390bcc5184eac856de4acd0e56e2
Dominic GOj <dominicgoj@gmail.com>
2025-11-12 16:53:11 +0100

    delte

--- homework/findlowest.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_excel("exports/training.xlsx")
-print(df.head())
-
-df['total_loss'] = df['Best train loss']+df['Best val loss']
-df_sorted = df.sort_values('total_loss')
-
-print(df.head())
